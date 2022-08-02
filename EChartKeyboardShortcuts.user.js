57022f98fde2d93d9638f7ce400e8889420b3ed2
sggongshow <87620031+sggongshow@users.noreply.github.com>
2022-08-02 13:55:39 -0700

    Update EChartKeyboardShortcuts.user.js

--- EChartKeyboardShortcuts.user.js
+++ EChartKeyboardShortcuts.user.js
@@ -3,9 +3,10 @@
 // @namespace      Gong Oscar
 // @description Various Echart shortcut buttons (Alt+ e,v,z,x,k,s,0,1,2,3,4,5,7). Set your own Measurement groupName and default population text.
 // @include        *oscarEncounter/IncomingEncounter.do?*
+// @include     *casemgmt/forward.jsp?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version			22.07.22.0
+// @version			22.08.02.0
 // ==/UserScript==
 
 //Brookswood
