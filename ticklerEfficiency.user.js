c0e4ba40d0129ec16e22fd4360322ec06a105180
sggongshow <87620031+sggongshow@users.noreply.github.com>
2024-07-04 19:55:11 -0700

    Update ticklerEfficiency.user.js

--- ticklerEfficiency.user.js
+++ ticklerEfficiency.user.js
@@ -1,24 +1,25 @@
 // ==UserScript==
 // @name        Tickler Efficiency
-// @namespace   GongOscar
+// @namespace   GongOscar - Clayton
 // @description Constant EForm Submit and Print button locations
 // @include     */ticklerAdd.jsp?*
 // @include     *ForwardDemographicTickler.do?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version	    22.07.22.0
+// @version	    24.07.04.1
 // ==/UserScript==
 
-//brookswood
+//clayton
 
 //wait window load first
 
-var myIDNum = '59'
+var myIDNum = '85'
+var UrgentTickUser = '69'
 
 window.addEventListener('load', function() {
-  
 
-  
+
+
   var MeBut = document.createElement('input');
   MeBut.type = 'button';
   MeBut.id = 'meButton'
@@ -27,7 +28,7 @@ window.addEventListener('load', function() {
   MeBut.onclick = meButtonFunc
   MeBut.setAttribute('style', 'width:80px;font-size:12px;padding:0px;position:fixed;top:60px;right:10; border-color:blue;');
 	document.body.appendChild(MeBut);
-  
+
   var HiBut = document.createElement('input');
   HiBut.type = 'button';
   HiBut.id = 'HiButton'
@@ -36,16 +37,16 @@ window.addEventListener('load', function() {
   HiBut.onclick = HiButtonFunc
   HiBut.setAttribute('style', 'width:80px;font-size:12px;padding:0px;position:fixed;top:80px;right:10; border-color:red;');
 	document.body.appendChild(HiBut);
-  
+
    //Default Tickler
   var taskAssign = $('select[name=task_assigned_to]')[0]
   console.log(taskAssign)
-  taskAssign.value = 20
-  
-  
+  taskAssign.value = 62
+
+
   //--------- select the textbox area so I can start typing immediately
   var textBox = $('textarea[name="textarea"]')
-  textBox.select()  
+  textBox.select()
 }, false);
 
 
@@ -54,26 +55,26 @@ document.addEventListener('keydown', function(theEvent) {
 	var theAltKey =theEvent.altKey;
 	var theCtrlKey = theEvent.ctrlKey;
 	var theShiftKey= theEvent.shiftKey;
-  
- 
-  
+
+
+
   switch(true){
       //Acknowledge  button
-    case theAltKey && theKey==='1': 
+    case theAltKey && theKey==='1':
       var subButton = $('input[type="button"][value*=Submit][value*=EXIT]')
       subButton.click()
 			break;
-      
-    case theAltKey && theKey==='w': 
+
+    case theAltKey && theKey==='w':
       meButtonFunc()
 			break;
-      
-    case theAltKey && theKey==='q': 
+
+    case theAltKey && theKey==='q':
       HiButtonFunc()
 			break;
-      
+
     default:
-      break; 
+      break;
   }
 
 
@@ -87,6 +88,6 @@ function meButtonFunc(){
 function HiButtonFunc(){
   var priorityAssign = $('select[name=priority]')[0]
   priorityAssign.value = 'High'
-  //var taskAssign = $('select[name=task_assigned_to]')[0]
-  //taskAssign.value = 40
+  var taskAssign = $('select[name=task_assigned_to]')[0]
+  taskAssign.value = UrgentTickUser
 }
