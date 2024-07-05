ac64ef160b027ef552e5aba8e8e23aeec33966a1
sggongshow <87620031+sggongshow@users.noreply.github.com>
2024-07-04 20:27:49 -0700

    Update TicklerViewMod.user.js

--- TicklerViewMod.user.js
+++ TicklerViewMod.user.js
@@ -2,31 +2,35 @@
 // @name        Tickler View Completed
 // @namespace   GongOscar
 // @description Constant EForm Submit and Print button locations
-// @include     */ticklerDemoMain.jsp?*
+// @include     */ticklerMain.jsp?*
 // @require     https://code.jquery.com/jquery-3.6.0.js
 // @grant       GM_addStyle
-// @version			22.05.17.2
+// @version			24.07.04.0
 // ==/UserScript==
 
 
 //wait window load first
 
-var myIDNum = '190'
+var myIDNum = '85'
 
 window.addEventListener('load', function() {
-  
+
   var compBut = document.createElement('input');
   compBut.type = 'button';
   compBut.id = 'compBut'
   compBut.name = 'compBut'
   compBut.value = 'COMPLETED'
   compBut.onclick = compButFunc
-  compBut.setAttribute('style', 'width:80px;font-size:12px;padding:0px;position:fixed;top:10px;right:10; background-color:cyan;');
-	document.body.appendChild(compBut);
-  
+  compBut.setAttribute('style', 'width:80px;font-size:12px;padding:0px; background-color:cyan;');
+
+  var bodyForAppend = document.querySelector('[class=HelpAboutLogout]').parentElement
+  bodyForAppend.appendChild(compBut)
+
+	//document.body.appendChild(compBut);
+
   //--------- select the textbox area so I can start typing immediately
   var textBox = $('textarea[name="textarea"]')
-  textBox.select()  
+  textBox.select()
 }, false);
 
 
@@ -37,16 +41,16 @@ document.addEventListener('keydown', function(theEvent) {
 	var theAltKey =theEvent.altKey;
 	var theCtrlKey = theEvent.ctrlKey;
 	var theShiftKey= theEvent.shiftKey;
-  
-  
+
+
   switch(true){
       //Acknowledge  button
-    case theAltKey && theKey==='1': 
+    case theAltKey && theKey==='1':
       compButFunc()
 			break;
 
     default:
-      break; 
+      break;
   }
 
 
@@ -58,6 +62,6 @@ function compButFunc(){
   selectList.value = "C"
   var reportBut = $('.mbttn[value*="Report"]')[0]
   reportBut.click()
-  
+
 }
 
