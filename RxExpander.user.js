1d43b25bae15b0c426dfc84691426a88d8c29d9f
sggongshow <87620031+sggongshow@users.noreply.github.com>
2024-11-08 10:13:34 -0800

    Create RxExpander.user.js

---
+++ RxExpander.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name        Rx Favorites Expander
+// @namespace   GongOscar
+// @description Favorites are cut off. fixed
+// @include     *oscarRx/choosePatient.do*
+// @include     *oscarRx/SearchDrug3*
+// @require     https://code.jquery.com/jquery-3.6.0.js
+// @grant       GM_addStyle
+// @version			24.11.08.0
+// ==/UserScript==
+
+//changelog
+//24.11.08.0. moved to Clayton Juno
+//23.06.25.0. Initial Code
+
+
+//wait window load first
+
+window.addEventListener('load', function() {
+
+  var FavArray
+
+  FavArray = document.querySelectorAll('a[onclick*="useFav2"]');
+  console.log(FavArray)
+
+  FavArray.forEach(element => {
+    const title = element.getAttribute('title');
+    element.innerText = title;
+  });
+
+}, false);
